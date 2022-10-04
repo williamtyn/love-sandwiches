43306f6fd2c6fd43c71d49a8f6362de53ab01644
williamtyn <william@tyner.se>
2022-10-04 15:00:19 +0000

    collect sales datas from 5 last markets in worksheet

--- run.py
+++ run.py
@@ -93,6 +93,19 @@ def calculate_surplus_data(sales_row):
     
     return surplus_data    
 
+def get_last_5_entries_sales():
+    """
+    Collects the 5 last entries of sales for each sandwich in worksheet
+    """
+    sales = SHEET.worksheet('sales')
+
+    columns = []
+    for ind in range(1,7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    return columns
+
+
 def main():
     """
     Run all program functions
@@ -102,6 +115,8 @@ def main():
     update_worksheet(sales_data, 'sales')
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, 'surplus')
+    
 
 print('Welcome to Love Sandwiches Data Automation')
-main()
+#main()
+sales_columns = get_last_5_entries_sales()
